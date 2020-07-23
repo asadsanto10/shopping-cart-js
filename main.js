6fbcd663198542e015de5e4231076d0273e6144d
asadsanto10 <santo.meridian@gmail.com>
2020-07-23 12:26:42 +0600

    solve increment decrement

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 let incrementItem =  document.getElementById('incrementItem');
 incrementItem.addEventListener('click', function () {
     incrementDecrement('itemValue', 'defultItemPrice', 'increment')
+    // const itemvalue = document.getElementById('itemValue').value;
+    // const totalItemQuantity = parseInt(itemvalue) + 1;
+    // //console.log(totalItemQuantity);
+    // document.getElementById('itemValue').value = totalItemQuantity;
+    // const defultItemPrice = document.getElementById('defultItemPrice').innerText;
+    // //console.log(defultItemPrice);
+    // const totalPrice = defultItemPrice * (totalItemQuantity / itemvalue);
+    // document.getElementById('defultItemPrice').innerText = totalPrice;
 });
 
 let decrementItem = document.getElementById('decrementItem');
@@ -23,20 +31,21 @@ decrementItem.addEventListener('click', function () {
 });
 
 
-function incrementDecrement(itemvaleID, defultItemPriceID, type) {
-    const itemvalue = document.getElementById(itemvaleID).value;
-    const totalItemQuantity = type == 'increment' ? parseFloat(itemvalue) + 1 : parseFloat(itemvalue) - 1;
-    //console.log(totalItemQuantity);
-    const totalPrice = totalItemQuantity / itemvalue;
+// incremenent decrement function
+function incrementDecrement(inputItemvale, defultItemPriceAmount, type) {
+    const itemvalue = document.getElementById(inputItemvale).value;
+    const totalItemQuantity = type == 'increment' ? parseInt(itemvalue) + 1 : parseInt(itemvalue) - 1;
+    const itemPriceAmount = document.getElementById(defultItemPriceAmount).innerText;
+    const newItemPrice = itemPriceAmount / itemvalue;
+    //console.log(itemPriceAmount);
     if (totalItemQuantity <= 1) {
-        const sd = document.getElementById(itemvaleID).value = 1;
-        document.getElementById(defultItemPriceID).innerText = sd * totalPrice;
+        document.getElementById(inputItemvale).value = 1;
+        var stt = document.getElementById(defultItemPriceAmount).innerText = newItemPrice;
+        //console.log(stt);
     }
     else{
-        document.getElementById(itemvaleID).value = totalItemQuantity;
-        const defultItemPrice = document.getElementById(defultItemPriceID).innerText;
-        //console.log(defultItemPrice);
-        const updatePrice = defultItemPrice * totalPrice;
-        document.getElementById(defultItemPriceID).innerText = updatePrice;
+        document.getElementById(inputItemvale).value = totalItemQuantity;
+        const updateItemPrice = newItemPrice * parseFloat(totalItemQuantity);
+        document.getElementById(defultItemPriceAmount).innerText = Math.round(updateItemPrice);
     }
 }
